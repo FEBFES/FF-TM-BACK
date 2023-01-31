2705636425bdecf39455c060013d75750d76e944
Ivan Shishkin <rayman.4@list.ru>
2023-01-31 22:17:46 +0300

    47 - Dashboard new fields

--- src/main/java/com/febfes/fftmback/dto/DashboardDto.java
+++ src/main/java/com/febfes/fftmback/dto/DashboardDto.java
@@ -3,8 +3,6 @@ package com.febfes.fftmback.dto;
 import java.util.List;
 
 public record DashboardDto(
-        String name,
-        String description,
         List<ColumnWithTasksDto> columns
 ) {
 
