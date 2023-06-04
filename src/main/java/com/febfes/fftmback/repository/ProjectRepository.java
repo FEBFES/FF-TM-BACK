910710ca9124219c6809291b51a30d7508c9f208
Ivan Shishkin <rayman.4@list.ru>
2023-06-04 11:39:16 +0300

    126 - fixed project "isFavourite"

--- src/main/java/com/febfes/fftmback/repository/ProjectRepository.java
+++ src/main/java/com/febfes/fftmback/repository/ProjectRepository.java
@@ -6,24 +6,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Set;
-
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
-    List<ProjectEntity> findAllByOwnerId(Long ownerId);
 
     @Query(
             nativeQuery = true,
             value = "SELECT COUNT(*) > 0  FROM favourite_project WHERE project_id = :projectId AND user_id = :userId"
     )
     Boolean isProjectFavourite(Long projectId, Long userId);
-
-    @Query(
-            nativeQuery = true,
-            value = "SELECT project_id FROM favourite_project WHERE user_id = :userId"
-    )
-    Set<Long> findAllFavouriteProjectIdsForUser(Long userId);
 
     @Modifying
     @Query(
@@ -38,5 +28,4 @@ public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
             value = "DELETE FROM favourite_project WHERE project_id = :projectId AND user_id = :userId"
     )
     void removeProjectFromFavourite(Long projectId, Long userId);
-
 }
