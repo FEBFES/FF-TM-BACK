36c601d9e39b5d45c9d220ebffd3c7e64865025d
Ivan Shishkin <rayman.4@list.ru>
2023-06-17 12:27:48 +0300

    63 - created migration for roles table

---
+++ src/main/java/com/febfes/fftmback/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.febfes.fftmback.repository;
+
+import com.febfes.fftmback.domain.dao.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+}
