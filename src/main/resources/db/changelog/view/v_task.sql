d2476aa4a8a0d1040888ddc827bdf440fafa2c92
Ivan Shishkin <rayman.4@list.ru>
2023-09-17 12:56:42 +0300

    173 - added auto views

--- src/main/resources/db/changelog/view/v_task.sql
+++ src/main/resources/db/changelog/view/v_task.sql
@@ -1,16 +1,23 @@
-select t.id                            as "id",
-       t.create_date                   as "createDate",
-       t.name                          as "name",
-       t.description                   as "description",
-       t.project_id                    as "projectId",
-       t.column_id                     as "columnId",
-       t.owner_id                      as "ownerId",
-       t.assignee_id                   as "assigneeId",
-       t.priority                      as "priority",
-       t.task_type_id                  as "taskTypeId",
-       (select count(*)
+--liquibase formatted sql
+
+--changeset auto-views:view-v_task runOnChange:true ignore:true
+
+create or replace view public.v_task
+as
+select t.id                            as "id"
+     , t.create_date                   as "createDate"
+     , t.name                          as "name"
+     , t.description                   as "description"
+     , t.project_id                    as "projectId"
+     , t.column_id                     as "columnId"
+     , t.owner_id                      as "ownerId"
+     , t.assignee_id                   as "assigneeId"
+     , t.priority                      as "priority"
+     , t.task_type_id                  as "taskTypeId"
+     , (select count(*)
         from file_entity fe
         where fe.entity_id = t.id
-          and fe.entity_type = 'TASK') as "filesCounter",
-       t.update_date                   as "updateDate"
+          and fe.entity_type = 'TASK') as "filesCounter"
+     , t.update_date                   as "updateDate"
 from public.task t
+;
